66a11e8f9c5c6fca103220b484fcecbeba95e6e9
Eduardo Arroyo <je.arroyo.velasco@gmail.com>
2025-09-26 14:13:45 -0600

    se suben cambios faltantes, ya que no funciona al descargar proyecto con otro dev

---
+++ src/test/java/mx/quality/santander/pages/AccordionPage.java
@@ -0,0 +1,14 @@
+package mx.quality.santander.pages;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AccordionPage {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/mx/quality/santander/utils/ConfigReader.java
@@ -0,0 +1,14 @@
+package mx.quality.santander.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConfigReader {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
